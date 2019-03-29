baea892b0b361e608020b3d8acb22f4273492ebf
Andrew Davison <andrew.davison@unic.cnrs-gif.fr>
2019-03-29 14:31:13 +0100

    Improved benchmark runner

--- benchmark_runner.py
+++ benchmark_runner.py
@@ -6,8 +6,12 @@ A script for running neuromorphic benchmarks
 from time import sleep
 import os
 from os.path import splitext, join
-from urlparse import urlparse
-from urllib import urlopen
+try:
+    from urlparse import urlparse
+    from urllib import urlopen
+except ImportError:
+    from urllib.parse import urlparse
+    from urllib.request import urlopen
 import json
 import tempfile
 import shutil
@@ -21,10 +25,18 @@ BENCHMARKS_SERVER = "https://benchmarks.hbpneuromorphic.eu"
 job_manager = nmpi.Client("testuser123",
                           password=os.environ["BENCHMARK_RUNNER_PASSWORD"])
 
-repositories = (
-    "https://github.com/CNRS-UNIC/hardware-benchmarks.git",
-    "https://github.com/apdavison/pynam.git",
-)
+queue_name_map = {
+    "SpiNNaker": "SpiNNaker",
+    "BrainScaleS": "BrainScaleS",
+    "Spikey": "Spikey",
+    "NEST": "nest-server"
+}
+
+repositories = [
+    #"https://github.com/CNRS-UNIC/hardware-benchmarks.git",
+    #"https://github.com/apdavison/pynam.git",
+    "https://github.com/hbp-unibi/SNABSuite_deploy",
+]
 
 
 def main(platform):
@@ -32,9 +44,10 @@ def main(platform):
         models = get_models(repository)
         for model in models:
             for task in model["tasks"]:
-                job = run_job(repository, task, platform)
-                results = get_results(job)
-                save_results(model, task, results, job)
+                if platform in task.get("target", []):
+                    job = run_job(repository, task, platform)
+                    results = get_results(job)
+                    save_results(model, task, results, job, platform)
 
 
 def get_models(repository):
@@ -47,11 +60,15 @@ def get_models(repository):
 
 
 def run_job(repository, task, platform):
+    queue_name = queue_name_map[platform]
+    config = {"pyNN_version": "0.8"}
+    if "config" in task:
+        config.update(task["config"].get(platform, {}))
     job_id = job_manager.submit_job(
                     source=repository,
                     command=task["command"],
-                    platform=platform,
-                    config={"pyNN_version": "0.8"},
+                    platform=queue_name,
+                    config=config,
                     collab_id=510)
     while job_manager.job_status(job_id) not in ("finished", "error"):
         sleep(10)
@@ -71,14 +88,9 @@ def get_results(job):
     return []
 
 
-system_name_map = {
-    "SpiNNaker": "SpiNNaker",
-    "nest-server": "NEST"
-}
-
-def save_results(model, task, results, job):
+def save_results(model, task, results, job, platform):
     data = {
-        "hardware_platform": system_name_map[job["hardware_platform"]],
+        "hardware_platform": platform,
         "experiment_description": job["code"],
         "model": model["model"]["name"],
         "task": task["name"],
@@ -86,11 +98,15 @@ def save_results(model, task, results, job):
         "job_id": job["id"],
         "results": results
     }
-    print data
+    print(data)
     response = requests.post(BENCHMARKS_SERVER + "/runs/", json=data, verify=False)
-    print response
+    if response.status_code in (200, 201):
+        print(response)
+    else:
+        raise Exception(response.text)
 
 
 if __name__ == "__main__":
-    main("nest-server")
+    #main("NEST")
     main("SpiNNaker")
+    #main("BrainScaleS")
